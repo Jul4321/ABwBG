4b4fd91abfe2d45a1abec9afc21d700b77e91f43
Jul4321 <piskorz.julka@gmail.com>
2024-10-15 11:00:09 +0200

    Add files via upload
    Rozwiązanie zadanie 1

---
+++ 2-IntroToR2/zadanie1.R
@@ -0,0 +1,20 @@
+
+  
+  4.  **W skrypcie napisz komentarze** opisujące funkcje poszczególnych paneli
+  #-------------------------  FUNKCJE POSZCZEGÓLNYCH PANELI ------------------
+  
+  #source (edytor skryptów)- słuzy do pisania skryptów a funkcje nie są automatycznie wykonywane
+  #console (konsola)- służy do wykonywania funkcji
+  #environment (środowisko)- wszystko co zapiszemy pojawaia się w środowisku(jako wartość/ramka danych)
+  #panel wyjściowy- ułatwienia dla Rstudio, importujemy tu dane
+
+  #--------------------------- MOTYW KOLORYSTYCZNY --------------------------
+  
+  # Wybrałam Textmate,najbardziej mi sie podoba.
+5.  **Znajdź w RStudio ustawienia** i **zmień motyw kolorystyczny** na taki, który najbardziej Ci odpowiada.
+
+6.  **Napisz w komentarzu**, jaki motyw wybrałeś/aś i dlaczego.
+
+7.  **Zapisz skrypt** jako `zadanie1.R` w swoim folderze.
+
+8.  **Prześlij plik do swojego repozytorium na GitHub**.
